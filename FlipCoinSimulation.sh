43ab4598d580f4d538b814276781f844308fa620
pranjal072 <pranjaltyagi97@gmail.com>
2020-07-20 18:18:54 +0530

    Limited max no. of wins to 21 and difference of score is displayed

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,8 +1,9 @@
-#!/bin/bash 
+#!/bin/bash
 
 echo "Flip Coin Simulation:"
 
-declare -A coinSide
+declare -A coinSide=( [Heads]=0 [Tails]=0 )
+
 for((i=0; i<40; i++))
 do
 	coin=$((RANDOM%2))
@@ -12,9 +13,26 @@ do
    else
 		((coinSide[Tails]++))
    fi
+	if [ ${coinSide[Heads]} -eq 21 ] || [ ${coinSide[Tails]} -eq 21 ]
+	then
+		break
+	fi
 done
+
 echo "Out of $i Coin Flips :"
 for side in ${!coinSide[@]}
 do
-	echo "$side won ${coinSide[$side]} times"
+   echo "$side won ${coinSide[$side]} times"
 done
+
+diff=$(( ${coinSide[Heads]}-${coinSide[Tails]} ))
+
+if [ $diff -gt 0 ]
+then
+	echo "Heads won by $diff points"
+elif [ $diff -lt 0 ]
+then
+	echo "Tails won by $(($diff*-1)) points"
+else
+	echo "ITS A TIE !!"
+fi
